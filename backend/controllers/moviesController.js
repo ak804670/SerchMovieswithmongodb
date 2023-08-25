46c0b6f4c835fca7e71198babf135691cf3678d9
ak804670 <ak804670@gmail.com>
2023-08-26 04:03:14 +0530

    delete end point added and delete button added

--- backend/controllers/moviesController.js
+++ backend/controllers/moviesController.js
@@ -57,6 +57,34 @@ const getFavorite = async (req, res) => {
     }
 }
 
+const deleteFavorite=async (req,res)=>{
+    try {
+        const {_id} = req.params;
 
 
-module.exports = { storeFavorite, getFavorite}
+        const data = await Favorite.findOneAndDelete({_id : _id })
+        console.log(data)
+        
+
+        
+        return res.status(200).json({
+            message:"Favorite Deleted successfully",
+            status:200,
+            data:data
+        })
+        
+
+        // return res.send(data)
+        
+        
+    } catch (error) {
+        console.log("error", error.message);
+        return res.status(503).json({
+            message:"something went wrong!",
+            status:503,
+            data:{}
+        })
+    }
+}
+
+module.exports = { storeFavorite, getFavorite, deleteFavorite}
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {storeFavorite, getFavorite}= require("../controllers/moviesController.js")
+const {storeFavorite, getFavorite, deleteFavorite}= require("../controllers/moviesController.js")
 
 
 router.post('/favorite', storeFavorite)
 router.get('/favorite', getFavorite)
+router.delete('/favorite/:_id', deleteFavorite)
 
 
 module.exports = router
